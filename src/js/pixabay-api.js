c8d3e41e2f3a2b43520ee08a3b5f2da4d7baa94b
EZH1809 <stellage.ua@gmail.com>
2024-03-03 19:53:25 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '42552421-44c442bdd9fc0080a82eeb482';
-
+const IMAGES_PER_PAGE = 15;
+let currentPage = 1;
 
 async function searchImages(query) {
-  const apiUrl = `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const apiUrl = `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${IMAGES_PER_PAGE}&page=${currentPage}`;
 
   try {
     const response = await axios.get(apiUrl);
@@ -20,7 +21,8 @@ async function searchImages(query) {
       if (!data.hits || data.hits.length === 0) {
       throw new Error('No images found for the given query.');
     }
-
+  // Увеличиваем значение текущей страницы для следующего запроса
+    currentPage++;
       return data.hits;
       
 
