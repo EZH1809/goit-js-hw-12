149a8ab59f8d0d0a254b283e2af80486f568c7dc
EZH1809 <stellage.ua@gmail.com>
2024-03-06 21:19:46 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -18,24 +18,21 @@ export const renderGallery = (images) => {
     return; // Прерываем выполнение функции, так как нет изображений
   }
 
-  // Создаем разметку для галереи
+ // Создаем разметку для галереи
   const markup = images
-    .map(image => {
-      const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
-      return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
-            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
-            </div>
+    .map(({ id, largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => `
+      <a class="gallery__link" href="${largeImageURL}">
+        <div class="gallery-item" id="${id}">
+          <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes</b>${likes}</p>
+            <p class="info-item"><b>Views</b>${views}</p>
+            <p class="info-item"><b>Comments</b>${comments}</p>
+            <p class="info-item"><b>Downloads</b>${downloads}</p>
           </div>
-        </a>
-      `;
-    })
+        </div>
+      </a>`
+    )
     .join('');
 
   // Вставляем разметку в контейнер галереи
